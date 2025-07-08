4e9bf6f02098e4aa6e36fdafb2d0e2dcfa874c36
Abdullah <153628735+abdo5elsherif@users.noreply.github.com>
2025-07-09 01:40:04 +0300

    Add files via upload

---
+++ Task_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static short ReadYear()
+        {
+            
+            Console.WriteLine( "\nPlease enter a year? ");
+            short Year = short.Parse( Console.ReadLine());
+            return Year;
+        }
+       static bool  isLeapYear(short Year)
+        { 
+            return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isLeapYear(ReadYear())?"Is Leap Year": "Not Leap Year");
+        }
+    }
+}
---
+++ Task_3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static string ReadString()
+        {
+            Console.WriteLine("Please Enter Number String");
+
+            string result = Console.ReadLine();
+            return result;
+        }
+
+        static int ConvertStringToInt(string input)
+        {
+            int result;
+            while (!int.TryParse(input, out result)) 
+            {
+
+            Console.WriteLine("This String Not Numbers");
+                input = ReadString();
+                
+            }
+            return result;
+        }
+        static int ReverseNumber(int num)
+        {
+            Console.WriteLine("String After Reversed: ");
+
+            int reversed = 0;
+
+            while (num != 0)
+            {
+                int digit = num % 10;         
+                reversed = reversed * 10 + digit;
+                num /= 10;                      
+            }
+
+            return reversed;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseNumber(ConvertStringToInt(ReadString())).ToString());
+        }
+    }
+}
