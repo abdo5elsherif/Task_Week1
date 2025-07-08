413936051453fe8c23a1e0c71b1988d0399e004d
Abdullah <153628735+abdo5elsherif@users.noreply.github.com>
2025-07-08 04:15:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task_2
+{
+    internal class Program
+    {
+        public static int MultiplicationAllMemmbers(int[] arr )
+        {
+            int result = arr
+              .Where(num => num != 0)     
+              .Aggregate(1, (all, num) => all * num);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 0, 4, 5 };
+
+            Console.WriteLine("Result : " + MultiplicationAllMemmbers(arr));
+
+        }
+    }
+}
